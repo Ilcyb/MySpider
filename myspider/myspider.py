eac59a535ab0913cd85f02005e1080297d567c92
ilcyb <hybmail1996@gmail.com>
2018-04-07 14:41:36 +0800

    add comments

--- myspider/myspider.py
+++ myspider/myspider.py
@@ -10,13 +10,27 @@ class MySpider:
         self.__session = requests.session()
 
     def setHeader(self, headers):
+        """set headers of session
+
+        Args:
+            headers: A dict contain headers.
+        """
         self.__headers = headers
 
     def getHeader(self):
+        """get headers of session
+        
+        Returns:
+            A dict contain session's headers.
+        """
         return self.__headers
 
     def saveCookies(self, filepath):
-        """save session's cookie to file"""
+        """save session's cookie to file
+
+        Args:
+            filepath: The filepath of cookie saved.
+        """
         cookies_dict = requests.utils.dict_from_cookiejar(
             self.__session.cookies)
         try:
@@ -26,7 +40,11 @@ class MySpider:
             raise SaveCookiesError(e)
 
     def loadCookiesFromFile(self, filepath):
-        """load cookie to session from file"""
+        """load cookie to session from file
+        
+        Args:
+            filepath: The file path to load the cookie.
+        """
         try:
             with open(filepath, 'rb') as cookies_file:
                 cookies_dict = load(cookies_file)
@@ -34,3 +52,35 @@ class MySpider:
                     cookies_dict)
         except Exception as e:
             raise LoadCookiesError(e)
+
+    def setProxy(self, proxy):
+        """set proxy for session.
+
+        Args:
+            proxy: A dict, like {"http": "http://10.10.1.10:3128"}.
+        """
+        self.__proxy = proxy
+
+    def setStartUrl(self, urls):
+        """set initial urls of the spider.
+
+        Args:
+            urls: A list of urls.
+        """
+        self.__start_urls = urls
+
+    def setAllowDomain(self, domain_list):
+        """set allow domains for the spider.
+
+        Args:
+            domain_list: A list of allow domians.
+        """
+        self.__allow_domain = domain_list
+
+    def getAllowDomain(self):
+        """get allow domains of the spider.
+
+        Returns:
+            A list of spider's allow domians.
+        """
+        return self.__allow_domain
